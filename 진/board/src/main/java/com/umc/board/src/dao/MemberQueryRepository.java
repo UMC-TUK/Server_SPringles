176a87d002edcb51a1f5f548bdc014c0094ffe7f
ohksj77 <ohksj77@gmail.com>
2023-05-28 13:54:21 +0900

    repository 코드 수정

--- 진/board/src/main/java/com/umc/board/src/dao/MemberQueryRepository.java
+++ 진/board/src/main/java/com/umc/board/src/dao/MemberQueryRepository.java
@@ -10,12 +10,6 @@ import java.util.UUID;
 
 public interface MemberQueryRepository {
     Optional<Member> findMemberByGithubId(String githubId);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE Member m SET m.refreshToken = :token WHERE m.id = :id")
     void updateRefreshToken(UUID id, String token);
-
-    @Query("SELECT m.refreshToken FROM Member m WHERE m.id = :id")
     String findRefreshTokenById(UUID id);
 }
