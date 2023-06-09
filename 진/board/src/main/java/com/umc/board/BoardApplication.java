a89d021600171675f27de51a8ca18270fd2aad05
ohksj77 <ohksj77@gmail.com>
2023-06-09 12:37:24 +0900

    chore: add jpa auditing annotation

--- 진/board/src/main/java/com/umc/board/BoardApplication.java
+++ 진/board/src/main/java/com/umc/board/BoardApplication.java
@@ -2,7 +2,9 @@ package com.umc.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardApplication {
 	public static void main(String[] args) {
